2c45b1a56dc81e904315729ce27e43f32e86be74
ishterev <ishterev@users.noreply.github.com>
2015-11-09 00:24:46 +0100

    test plotting

---
+++ PDOF/plotting/pyplot_two_subplots.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+
+'''
+
+import matplotlib.pyplot as plt
+plt.figure(1)                # the first figure
+plt.subplot(211)             # the first subplot in the first figure
+plt.plot([1,2,3])
+plt.subplot(212)             # the second subplot in the first figure
+plt.plot([4,5,6])
+
+
+plt.figure(2)                # a second figure
+plt.plot([4,5,6])            # creates a subplot(111) by default
+
+plt.figure(1)                # figure 1 current; subplot(212) still current
+plt.subplot(211)             # make subplot(211) in figure1 current
+plt.title('Easy as 1,2,3')   # subplot 211 title
+
+'''
